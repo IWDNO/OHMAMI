94c7fc041d05dddd1da5f3f39f0bbf6d70d4c75f
IWDNO <zaharovpavel10133@gmail.com>
2025-10-11 09:45:58 +0300

    initial commit

---
+++ ohmami_app/README.md
@@ -0,0 +1,3 @@
+# ohmami_app
+
+A new Flutter project.
---
+++ ohmami_app/android/app/src/main/kotlin/com/example/ohmami_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ohmami_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
